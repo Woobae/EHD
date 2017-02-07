59551c54e50eaae43542e6f44a8bc70cd8376a58
Woobae <kimbobae@hotmail.com>
2017-02-08 00:40:25 +0900

    Commit

---
+++ Launcher/Launcher/frmMain.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Launcher
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.mainWeb = new System.Windows.Forms.WebBrowser();
+            this.mainSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.btnCollaps = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.mainSplitContainer.Panel1.SuspendLayout();
+            this.mainSplitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainWeb
+            // 
+            this.mainWeb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainWeb.Location = new System.Drawing.Point(0, 0);
+            this.mainWeb.MinimumSize = new System.Drawing.Size(20, 20);
+            this.mainWeb.Name = "mainWeb";
+            this.mainWeb.Size = new System.Drawing.Size(984, 522);
+            this.mainWeb.TabIndex = 0;
+            this.mainWeb.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.mainWeb_DocumentCompleted);
+            this.mainWeb.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.mainWeb_Navigating);
+            this.mainWeb.NewWindow += new System.ComponentModel.CancelEventHandler(this.mainWeb_NewWindow);
+            // 
+            // mainSplitContainer
+            // 
+            this.mainSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainSplitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.mainSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.mainSplitContainer.Name = "mainSplitContainer";
+            this.mainSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // mainSplitContainer.Panel1
+            // 
+            this.mainSplitContainer.Panel1.Controls.Add(this.button2);
+            this.mainSplitContainer.Panel1.Controls.Add(this.button1);
+            this.mainSplitContainer.Panel1.Controls.Add(this.btnCollaps);
+            this.mainSplitContainer.Panel1.Controls.Add(this.mainWeb);
+            // 
+            // mainSplitContainer.Panel2
+            // 
+            this.mainSplitContainer.Panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("mainSplitContainer.Panel2.BackgroundImage")));
+            this.mainSplitContainer.Size = new System.Drawing.Size(984, 611);
+            this.mainSplitContainer.SplitterDistance = 522;
+            this.mainSplitContainer.SplitterWidth = 1;
+            this.mainSplitContainer.TabIndex = 1;
+            // 
+            // btnCollaps
+            // 
+            this.btnCollaps.Location = new System.Drawing.Point(0, 505);
+            this.btnCollaps.Name = "btnCollaps";
+            this.btnCollaps.Size = new System.Drawing.Size(70, 20);
+            this.btnCollaps.TabIndex = 2;
+            this.btnCollaps.Text = "Collapse";
+            this.btnCollaps.UseVisualStyleBackColor = true;
+            this.btnCollaps.Click += new System.EventHandler(this.btnCollaps_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(798, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(879, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(984, 611);
+            this.Controls.Add(this.mainSplitContainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launcher";
+            this.mainSplitContainer.Panel1.ResumeLayout(false);
+            this.mainSplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser mainWeb;
+        private System.Windows.Forms.SplitContainer mainSplitContainer;
+        private System.Windows.Forms.Button btnCollaps;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Launcher/Launcher/frmMain.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Launcher
+{
+    public partial class frmMain : Form
+    {
+        private string StartGameHeader = "NToGame://";
+        private bool SplitPanel2Collapsed = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            mainWeb.Navigate("http://www.naver.com");
+
+            //mainWeb.Navigating += new WebBrowserNavigatingEventHandler(mainWeb_Navigating);
+        }
+
+        private void mainWeb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void mainWeb_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            if (e.Url.ToString().StartsWith(StartGameHeader))
+            {
+                e.Cancel = true;
+                StartGame(e.Url.ToString().Replace(StartGameHeader, ""));
+            }
+        }
+
+        private void StartGame(string gmaeUrl)
+        {
+        }
+
+        private void btnCollaps_Click(object sender, EventArgs e)
+        {
+            SplitPanel2Collapsed = !SplitPanel2Collapsed;
+            mainSplitContainer.Panel2Collapsed = SplitPanel2Collapsed;
+            if (SplitPanel2Collapsed)
+            {
+                btnCollaps.Location = new Point(0, 595);
+                btnCollaps.Text = "Show";
+            }
+            else
+            {
+                btnCollaps.Location = new Point(0, 505);
+                btnCollaps.Text = "Hide";
+            }
+        }
+
+        private void mainWeb_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void ResizeMe(int Width, int Height, int targetWidth, int targetHeight)
+        {
+            //int diffWidth = Math.Abs(Width - targetWidth);
+            //int diffHeight = Math.Abs(Height - targetHeight);
+            //int loopCount = diffWidth > diffHeight ? diffWidth : diffHeight;
+
+            //while (loopCount > 0)
+            //{
+            //    loopCount--;
+
+            //    if (Width > targetWidth) Width = Width - 5;
+            //    if (targetWidth > Width) Width = Width + 5;
+
+            //    if (Height > targetHeight) Height = Height - 5;
+            //    if (targetHeight > Height) Height = Height + 5;
+
+            //    this.Width = Width;
+            //    this.Height = Height;
+
+            //    //MoveToCenter();
+            //    //Application.DoEvents();
+            //}
+
+            this.Width = targetWidth;
+            this.Height = targetHeight;
+
+            MoveToCenter();
+            Application.DoEvents();
+        }
+
+        private void MoveToCenter()
+        {
+            this.Location = new Point((Screen.PrimaryScreen.WorkingArea.Width - this.Width) / 2, (Screen.PrimaryScreen.WorkingArea.Height - this.Height) / 2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ResizeMe(this.Width, this.Height, 1500, 1000);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ResizeMe(this.Width, this.Height, 1000, 650);
+        }
+    }
+}
